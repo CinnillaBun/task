0456e2c05ae71e1a85109cfd8825729834f3b470
Veronika Koshkareva <100349937+CinnillaBun@users.noreply.github.com>
2022-06-09 14:41:42 +0400

    Update main.cpp
    Добавлен вывод 10-го числа фибоначчи

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+int f(int n) {
+    if ( n == 1 || n == 2) return (n - 1); 
+    return f(n-1) + f(n-2); 
+}
+
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
+    std::cout <<f(10)<<std::endl;  
     return 0;
 }
